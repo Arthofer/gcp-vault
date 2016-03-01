dbb4d1c32c84da72cc069432850cc4de25a9b05f
Xu Wang <xuwang@gmail.com>
2016-03-01 12:46:41 -0800

    fix the provider.tf DEPRECATED attr issue

--- tf/provider.tf
+++ tf/provider.tf
@@ -1,6 +1,6 @@
 # Configure the Google Cloud provider
 provider "google" {
-  credentials = "${file("account.json")}"
-  project     = "vault-20160301"
-  region      = "us-central1"
+    credentials = "${file("${var.account_file}")}"
+    project = "${var.project}"
+    region = "${var.region}"
 }
